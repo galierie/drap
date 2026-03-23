ca1fc1f23f7fe048e7ab9eea156ddfa6168b19a0
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2026-03-24 00:54:16 +0800

    refactor: adjust bottom nav styles to be consistent with desktop navs

--- src/lib/components/bottom-nav.svelte
+++ src/lib/components/bottom-nav.svelte
@@ -1,8 +1,8 @@
 <script>
-  import HouseIcon from '@lucide/svelte/icons/house';
+  import FlaskConicalIcon from '@lucide/svelte/icons/flask-conical';
+  import HomeIcon from '@lucide/svelte/icons/home';
   import MenuIcon from '@lucide/svelte/icons/menu';
 
-  import Logo from '$lib/assets/logo-DRAP-icon-colored.svg';
   import { cn } from '$lib/components/ui/utils';
   import { page } from '$app/state';
   import { resolve } from '$app/paths';
@@ -11,45 +11,43 @@
 </script>
 
 <nav
-  class="flex w-full items-center justify-between gap-2 bg-background/90 px-10 py-2 backdrop-blur-md"
+  class="flex w-full items-center justify-between gap-2 border-t border-border bg-background/90 px-10 py-2 backdrop-blur-md"
 >
-  <div class="flex w-1/3 place-content-center items-center">
+  <div class="flex size-12 place-content-center items-center">
     <button
-      class="group flex w-2/5 place-content-center rounded-lg py-2 active:bg-primary dark:active:bg-white"
+      class="flex place-content-center rounded-lg p-3 active:bg-sidebar-accent active:text-sidebar-accent-foreground"
       onclick={_ => {
         sidebar.toggle();
       }}
     >
-      <MenuIcon class="size-6 group-active:text-white dark:group-active:text-black" />
+      <MenuIcon size={24} />
       <span class="sr-only">Toggle Sidebar</span>
     </button>
   </div>
 
-  <div class="flex w-1/3 place-content-center items-center">
+  <div class="flex size-12 place-content-center items-center">
     <div
       class={cn(
-        'flex w-2/5 place-content-center rounded-lg py-2',
-        page.url.pathname.startsWith('/dashboard') ? 'bg-primary dark:bg-white' : 'bg-transparent',
+        'flex place-content-center rounded-lg p-3',
+        page.url.pathname.startsWith('/dashboard')
+          ? 'bg-sidebar-accent text-sidebar-accent-foreground'
+          : 'text-foreground',
       )}
     >
       <a href={resolve('/dashboard/')}>
-        <HouseIcon
-          class={cn(
-            'size-6',
-            page.url.pathname.startsWith('/dashboard')
-              ? 'text-white dark:text-black'
-              : 'text-primary',
-          )}
-        />
+        <FlaskConicalIcon size={24} />
+        <span class="sr-only">Go to Dashboard</span>
       </a>
-      <span class="sr-only">Go to Dashboard</span>
     </div>
   </div>
 
-  <div class="flex w-1/3 place-content-center items-center">
-    <a href={resolve('/')}>
-      <img src={Logo} alt="DRAP Logo" class="size-6" />
+  <div class="flex size-12 place-content-center items-center">
+    <a
+      href={resolve('/')}
+      class="flex place-content-center rounded-lg p-3 active:bg-sidebar-accent active:text-sidebar-accent-foreground"
+    >
+      <HomeIcon size={24} />
+      <span class="sr-only">Go to Home</span>
     </a>
-    <span class="sr-only">Go to Home</span>
   </div>
 </nav>
